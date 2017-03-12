bc9c55f471b30fa03742b27e9da00e7432b281d7
ubiramen <ubiramen@users.noreply.github.com>
2017-03-12 03:41:41 -0700

    Update gauge.js

--- gauge.js
+++ gauge.js
@@ -1,127 +1,168 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * *
+ * Battery widget plugin for freeboard.
+ * Author: Vikas Lamba, https://github.com/vkylamba
+ * Licensed under the MIT (http://raphaeljs.com/license.html) license.    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * *
+*/
+(function () {
 
-window.dyngaugeID = 0;
-(function() {
-    var dynamicGaugeWidget = function (settings) {
-        var self = this;
-        var thisDynGaugeID = "dyngauge-" + window.dyngaugeID++;
-        var titleElement = $('<h2 class="section-title"></h2>');
-        var gaugeElement = $('<div id="' + thisDynGaugeID + '"></div>');
+  var batteryWidget = function (settings) {
 
-        var gaugeObject;
-        var rendered = false;
+    var self = this;    
+    var currentSettings = settings;
+    var htmlElement;
+    var data = 99;//
+    var battHeight = currentSettings.batteryHeight;
+    var battWidth = currentSettings.batteryWidth;
 
-        var currentSettings = settings;
-        var currentValues = {
-            value: 0,
-            min_value: 0,
-            max_value: 0,
-            level_colors: ['#f45b5b', '#f9c802', '#a9d70b', '#55BF3B'],
-        };
+    this.render = function (element) {
+      console.log('render');
 
-        function createGauge() {
-            if (!rendered) {
-                return;
-            }
 
-            gaugeElement.empty();
+      var battDiv = '<div id="'+currentSettings.id+'" align="center">\
+						 </div>';
+      console.log(battDiv);
+      htmlElement = $(battDiv);
+      $(element).append(htmlElement);
+    }
 
-            gaugeObject = new JustGage({
-                id: thisDynGaugeID,
-                value: currentValues.value,
-                min: currentValues.min_value,
-                max: currentValues.max_value,
-                label: currentSettings.units,
-                showInnerShadow: false,
-                valueFontColor: "#d3d4d4",
-                levelColors: currentValues.level_colors,
-            });
-        }
+    this.onSettingsChanged = function (newSettings) {
+      currentSettings = newSettings;
+    }
 
-        this.render = function (element) {
-            rendered = true;
-            $(element).append(titleElement).append($('<div class="gauge-widget-wrapper"></div>').append(gaugeElement));
-            createGauge();
-        }
+    this.onCalculatedValueChanged = function (settingName, newValue) {
+      console.log('onCalculatedValueChanged for ' + settingName);
 
-        this.onSettingsChanged = function (newSettings) {
-            if (newSettings.units != currentSettings.units) {
-                currentSettings = newSettings;
-                createGauge();
-            }
-            else {
-                currentSettings = newSettings;
-            }
+      if (settingName == 'data')
+        data = newValue;
+        
 
-            titleElement.html(newSettings.title);
-        }
+      //render the battery
+      htmlElement.empty();
+      //Code to draw battery
+      var canvas = '<canvas id="canvas_'+currentSettings.id+'" align="center" width="'+currentSettings.batteryWidth+'" height="'+currentSettings.batteryHeight+'" style="position: center; overflow: visible;">\
+							HTML5 Canvas not supported\
+						 </canvas>';
+		htmlElement.append(canvas);
+      
+      canvas  = document.getElementById('canvas_'+currentSettings.id);
+      
 
-        this.onCalculatedValueChanged = function (settingName, newValue) {
-            currentValues[settingName] = newValue;
-            if (!_.isUndefined(gaugeObject)) {
-                if (settingName == 'value') {
-                    gaugeObject.refresh(Number(newValue));
-                } else {
-                    createGauge();
-                }
-            }
-        }
+      var start_x = 0, start_y = 0;
+		var battery_height = currentSettings.batteryHeight;
+		var battery_width = currentSettings.batteryWidth;
+		var cap_height = battery_height/16;
+		var cap_width = battery_width/3;
+		var line_width = 2;
+		var margin = 1;
+		var number_of_blocks = 5;
+		
+		var context = canvas.getContext("2d");
+		context.globalAlpha = 1;
+		
+		//Drawing the battery cap
+		context.fillStyle = "#ffffff";
+  		context.fillRect(start_x + battery_width/2 - cap_width/2, start_y, cap_width, cap_height);
+  		
+  		//Clear the battery area first
+		context.fillStyle = "#ffffff";
+		context.fillRect(start_x, start_y + cap_height, battery_width, battery_height-cap_height);
+  		//Drawing body outline
+  		context.strokeStyle = "#000f00";
+		context.lineWidth   = line_width;
+		context.strokeRect(start_x, start_y + cap_height, battery_width, battery_height-cap_height);
+		//context.fillStyle = "#000000";
+		//context.fillRect(start_x + margin, start_y + cap_height + margin, battery_width - margin, battery_height-cap_height-margin);
+		//Filling body
+		var block_width = battery_width - 2*(line_width + margin);
+		var block_height = Math.ceil((battery_height - cap_height - 2*(line_width + margin))/number_of_blocks) - 2*margin;
+		var block_start_y = start_y + battery_height - line_width - margin;
+		
+		var last_block = Math.ceil(number_of_blocks*data[1]/100);
+		var green_val = Math.floor(data[1]*255/100);//<49?255:0;
 
-        this.onDispose = function () {
-        }
 
-        this.getHeight = function () {
-            return 5;
-        }
+		var red_val = 255 - green_val;
+		var linearGradient1 = context.createLinearGradient(start_x + line_width + margin, 
+																			start_y + cap_height + line_width + margin, 
+																			start_x + line_width + margin + block_width, 
+																			start_y + cap_height + line_width + margin);
+		linearGradient1.addColorStop(0, 'rgb('+red_val+', '+green_val+', 0)');
+		linearGradient1.addColorStop(0.5, 'rgb(0, 0, 0)');
+		linearGradient1.addColorStop(1, 'rgb('+red_val+','+green_val+', 0)');
+		
+		for(var i=1;i <= last_block; i++)
+		{
+			var y_pos = start_y + block_start_y - i*(block_height + margin);
+			
+			context.fillStyle = linearGradient1;
+			context.fillRect(start_x + line_width + margin, y_pos, block_width, block_height);
+			//string += "("+y_pos +","+block_height+")";
+		}
+		
+		//alert(string);
+		context.font      = "16px Verdana";
+		context.fillStyle = "#ff0000";
+		context.fillText(data[0]+"V", start_x + battery_width/4, start_y + battery_height/2-15, battery_width);
+		context.fillText(data[1]+"%", start_x + battery_width/4, start_y + battery_height/2+10, battery_width);
+		context.fillText(data[2]+"℃", start_x + battery_width/4, start_y + battery_height/2+37, battery_width);
+      
+     
+      
+    }
 
-        this.onSettingsChanged(settings);
-    };
+    this.onDispose = function () {
+    }
 
-    freeboard.loadWidgetPlugin({
-        type_name: "dyngauge",
-        display_name: "DynamicGauge",
-        "external_scripts" : [
-            "/freeboard-ui/plugins/thirdparty/raphael.2.1.0.min.js",
-            "/freeboard-ui/plugins/thirdparty/justgage.1.0.1.js"
-        ],
-        settings: [
-            {
-                name: "title",
-                display_name: "Title",
-                type: "text"
-            },
-            {
-                name: "value",
-                display_name: "Value",
-                type: "calculated",
-                size : 7
-            },
-            {
-                name: "units",
-                display_name: "Units",
-                type: "text"
-            },
-            {
-                name: "min_value",
-                display_name: "Minimum",
-                type: "calculated",
-                default_value: 0
-            },
-            {
-                name: "max_value",
-                display_name: "Maximum",
-                type: "calculated",
-                default_value: 1000
-            },
-            {
-                name: "level_colors",
-                display_name: "Level colors",
-                type: "calculated",
-                default_value: "return ['#f45b5b', '#f9c802', '#a9d70b', '#55BF3B']"
-            }
-        ],
-        newInstance: function (settings, newInstanceCallback) {
-            newInstanceCallback(new dynamicGaugeWidget(settings));
-        }
-    });
+    this.getHeight = function () {
+      return Number(currentSettings.height);
+    }
+
+    this.onSettingsChanged(settings);
+  };
+
+  freeboard.loadWidgetPlugin({
+    "type_name": "batteryWidget",
+    "display_name": "Battery",    
+    "fill_size": true,  
+    "settings": [
+      {
+        "name": "id",
+        "display_name": "id",
+        "default_value": "battery1",
+        "description": "DOM element id of the battery (must be unique)"
+      },        
+      {
+        "name": "data",
+        "display_name": "Battery Data array",
+        "type": "calculated",
+        "description": "The data list of size 3 containing voltage, battery percentage, and battery temperature. ex: [12.3, 45, 34]"
+      },    
+      {
+        "name": "batteryHeight",
+        "display_name": "Battery Height (px)",
+        "type": "number",
+        "default_value": 120,
+        "description": "battery height in pixels"
+      },
+      {
+        "name": "batteryWidth",
+        "display_name": "Battery Widgth (px)",
+        "type": "number",
+        "default_value": 80,
+        "description": "battery width in pixels"
+      },      
+      {
+        "name": "height",
+        "display_name": "Height Blocks",
+        "type": "number",
+        "default_value": 2,
+        "description": "A height block is around 60 pixels"
+      }
+    ],
+    newInstance: function (settings, newInstanceCallback) {
+      newInstanceCallback(new batteryWidget(settings));
+    }
+  });
 
 }());
